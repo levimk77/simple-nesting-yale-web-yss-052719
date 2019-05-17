ee7ba5221cc2b78118e8287050e7993c84ab2162
Elijah Florence <efloren@siue.edu>
2019-05-17 22:42:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     programmer_hash[:grace_hopper]
+     programmer_hash[:alan_kay][known_for]
 end
 
 def dennis_ritchies_language
@@ -57,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[languages][0]
 end
 
 def adding_matz
